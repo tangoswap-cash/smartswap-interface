6a2673c311061c950f18900229a896c507d96f7e
BitcoinIsCash <tangoswapcash@gmail.com>
2022-06-09 10:27:46 +0200

    feat: wip

--- src/features/trade/Header.tsx
+++ src/features/trade/Header.tsx
@@ -54,7 +54,7 @@ const ExchangeHeader: FC<ExchangeHeaderProps> = ({ input, output, allowedSlippag
           }}
         >
           <a className="flex items-center justify-center px-4 text-base font-medium text-center rounded-md text-secondary hover:text-high-emphesis ">
-            {i18n._(t`SmartSwap`)} <sup>NEW</sup>
+            {i18n._(t`SmartSwap`)}
           </a>
         </NavLink> */}
       </div>
